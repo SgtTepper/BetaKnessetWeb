dbe892457527fdc2f7a98c47b5485511efde3cfb
SgtTepper <yotepper@microsoft.com>
2021-02-28 19:40:58 +0200

    Remove email share button + center share buttons

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FacebookIcon, TwitterIcon, WhatsappIcon, EmailIcon, FacebookShareButton, TwitterShareButton, WhatsappShareButton, EmailShareButton } from "react-share";
+import { FacebookIcon, TwitterIcon, WhatsappIcon, FacebookShareButton, TwitterShareButton, WhatsappShareButton } from "react-share";
 
 export default function ShareButtons({Text, Speaker, FilePath}) {
     const size = 18
@@ -8,15 +8,12 @@ export default function ShareButtons({Text, Speaker, FilePath}) {
             <FacebookShareButton url={FilePath} quote={`${Speaker}: "${Text}"`}>
                 <FacebookIcon size={size} round />
             </FacebookShareButton>
-            <TwitterShareButton url={FilePath} title={`${Speaker}: "${Text}"`.substr(0, 220) + '...'}>
+            <TwitterShareButton url={FilePath} title={`${Speaker}: "${Text}"`}>
                 <TwitterIcon size={size} round />
             </TwitterShareButton>
             <WhatsappShareButton url={FilePath} title={`${Speaker}: "${Text}"`} separator=" - ">
                 <WhatsappIcon size={size} round />
             </WhatsappShareButton>
-            <EmailShareButton url={FilePath} subject={"Cool quote i found on BetaKnesset"} body={`${Speaker}: "${Text}"`}>
-                <EmailIcon size={size} round />
-            </EmailShareButton>
         </div>
         )
 }
