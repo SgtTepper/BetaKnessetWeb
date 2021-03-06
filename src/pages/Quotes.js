96cce88091f8c5474d511de2a52f82b28ebda19a
Omer Perry <thebne@gmail.com>
2021-03-06 19:47:43 +0200

    show total search quotes count

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -9,11 +9,37 @@ import config from '../config'
 import { useBigScreen, useQuery } from '../utils'
 import defaultTopics from '../defaultTopics'
 
+const maxResults = 20
+
 const Quotes = React.memo(function () {
     const [loading, setLoading] = useState(false)
     const randomTopic = defaultTopics[Math.floor(Math.random() * defaultTopics.length)]
     const isBigScreen = useBigScreen()
     const prefix = isBigScreen ? "כל נושא שהוא, " : ""
+
+    const [data, setData] = useState({})
+    const query = useQuery()
+
+    useEffect(() => {
+        (async () => {
+            setData({})
+            if (!query.length)
+                return
+            setLoading(true)
+            try {
+                const res = await (await fetch(`${config.server}/Quotes?keyword=${query}`)).json()
+                setData(res)
+            } catch(e) {
+                // TODO handle errors - (ex: when multi term search is empty)
+                console.error(e)
+                setData({})
+            } finally {
+                setLoading(false)
+            }
+        })()
+    }, [query])
+
+    console.log(query, data)
 
     return (
         <ScrollPage limit id='quotes' parentStyle={{backgroundColor: '#223388'}}>
@@ -21,16 +47,45 @@ const Quotes = React.memo(function () {
                 position: 'relative', 
                 height: '100%', 
                 width: '100%', 
-                overflowY: 'auto', 
                 zIndex: 2, 
-                display: 'flex', 
-                placeContent: 'center'
             }}>
-                <div style={{maxWidth: '850px', width: '100%'}}>
-                    <div style={{padding: '.5em'}}>
-                        <WhiteQuotesSearch placeholder={`${prefix}לדוגמא "${randomTopic}"`} />
-                        <Loader show={loading} />
-                        <QuoteView loading={loading} setLoading={setLoading} />
+                <div style={{
+                    width: '100%',
+                    display: 'flex', 
+                    flexDirection: 'column',
+                    placeContent: 'stretch',
+                    height: '100%',
+                }}>
+                    <div style={{display: 'flex', placeContent: 'center', padding: '.5em'}}>
+                        <div style={{maxWidth: '850px', width: '100%'}}>
+                            <WhiteQuotesSearch placeholder={`${prefix}לדוגמא "${randomTopic}"`} />
+                            {data.count > 0 &&
+                                <Typography style={{textAlign: 'left', color: "#ddd"}}>
+                                    {data.count === 1
+                                        ? "תוצאה אחת"
+                                        : `${numberWithCommas(data.count)} תוצאות`
+                                    }
+                                    {data.count > maxResults && ` (מציג ${maxResults} אחרונות)`}
+                                </Typography>
+                            }
+                        </div>
+                    </div>
+                    <div style={{overflowY: 'auto', display: 'flex', placeContent: 'center', padding: '.5em'}}>
+                        <div style={{maxWidth: '850px', width: '100%'}}>
+                            <Loader show={loading} />
+                            {(!loading && query.length && data.count === 0) ?
+                                <>
+                                    <Typography 
+                                        variant="h6" 
+                                        style={{fontStyle: 'italic', color: 'white', paddingTop: '1em', textAlign: 'center'}}
+                                    >
+                                        לא נמצאו ציטוטים בנושא {query}
+                                    </Typography>
+                                </>
+                            : null}
+                            {(!loading && data.count > 0) &&
+                                <QuoteView query={query} quotes={data.quotes} />}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -39,46 +94,15 @@ const Quotes = React.memo(function () {
 })
 export default Quotes
 
-const QuoteView = React.memo(function ({loading, setLoading}) {
-    const [data, setData] = useState([])
-    const query = useQuery()
-
-    useEffect(() => {
-        (async () => {
-            setData([])
-            if (!query.length)
-                return
-            setLoading(true)
-            try {
-                const res = await (await fetch(`${config.server}/Quotes?keyword=${query}`)).json()
-                setData(res.quotes)
-            } catch(e) {
-                // TODO handle errors - (ex: when multi term search is empty)
-                console.error(e)
-                setData([])
-            } finally {
-                setLoading(false)
-            }
-        })()
-    }, [query, setLoading])
-
-    if (loading)
-        return null
-
-    if (!data.length) 
-        return (
-            <Typography 
-                variant="h6" 
-                style={{fontStyle: 'italic', color: 'white', paddingTop: '1em', textAlign: 'center'}}
-            >
-                לא נמצאו ציטוטים בנושא {query}
-            </Typography>
-        )
-
+const QuoteView = React.memo(function ({query, quotes}) {
     return (
-        <Chat items={data.map(d => ({
+        <Chat items={quotes.map(d => ({
             highlight: query,
             ...d,
         }))} />
-    );
+    )
 })
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
