7589511d2633645a350a20e756b34a2a496418e5
Omer Perry <thebne@gmail.com>
2021-03-06 03:28:55 +0200

    fix warnings

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import Typography from '@material-ui/core/Typography'
 
 import { ScrollPage } from '../components/ScrollableView'
