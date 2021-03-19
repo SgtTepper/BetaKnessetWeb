f223de46e0e860a82e393918d58edd9a0d63fcca
Omer Perry <thebne@gmail.com>
2021-03-19 13:16:29 +0200

    fix frames on safari

--- src/pages/FrameApp.js
+++ src/pages/FrameApp.js
@@ -5,18 +5,7 @@ import { ScrollPage } from '../components/ScrollableView'
 const FrameApp = React.memo(function ({url, title}) {
     return (
         <ScrollPage limit>
-            <div style={{
-                position: 'relative', 
-                height: '100%', 
-                width: '100%', 
-                overflow: 'hidden',
-                zIndex: 2, 
-                display: 'flex', 
-                flexDirection: 'column',
-                placeContent: 'stretch',
-            }}>
                 <iframe title={title} src={url} style={{flexGrow: 1, border: 0}} />
-            </div>
         </ScrollPage>
     )
 })
