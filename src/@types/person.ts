0de0388b63472232fec36ca4f5208df7f2d9899d
yedidya rashi <Yedidyarashi@gmail.com>
2023-07-27 16:12:24 +0300

    refactor(migrating-to-ts): migrate utils

---
+++ src/@types/person.ts
@@ -0,0 +1,10 @@
+export interface Person {
+    PersonID: number;
+    FirstName: string;
+    LastName: string;
+    FactionName: string;
+    imgPath: string | null;
+    StartDate: string;
+    FinishDate: string | null;
+    KnessetNum: number;
+}
